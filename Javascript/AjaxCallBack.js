6b8a37c0c39479baa496dec376574a3e0b0819c0
PAVAN903 <navap903@gmail.com>
2023-05-15 13:05:13 +0530

    Added json and database

---
+++ Javascript/AjaxCallBack.js
@@ -0,0 +1,56 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+function showTime(){
+    const date = new Date();
+    return date.getHours + " Hrs, " + date.getMinutes + " Min " + date.getSeconds() + "Secs";
+}
+
+//callback
+function makeAJAXCall(methodType, url, callback, async = true, data = null){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        console.log(methodType+" State Change Called. Ready State: "+ xhr.onreadystatechange+" Status: "+xhr.status);
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);            
+        } else if (xhr.status >= 400){
+            console.log("Handle 400 client Error or 500 server Error");
+        }
+    }
+    xhr.open(methodType, url, async);
+    xhr.send();
+    console.log(methodType+" request send to server at "+showTime());
+}
+//promise
+function promiseCall(methodType, url, callback, async = true, data = null){
+    return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function()
+    xhr.onload = function(){
+        console.log(methodType+" State Change Called. Ready State: "+ xhr.onreadystatechange+" Status: "+xhr.status);
+        if (xhr.readyState == 4) {
+            resolve(xhr.responseText);            
+        } else if (xhr.status >= 400){
+            reject({
+                status : xhr.status,
+                statusText : xhr.statusText
+            });
+            console.log("Handle 400 client Error or 500 server Error");
+        }
+    }
+    xhr.onerror = function() {
+        reject({
+            status : this.status,
+            statusText : xhttp.statusText
+        });
+    };
+    xhr.open(methodType, url, async);
+    if(data) {
+        console.log(JSON.stringify(data));
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    }
+    else {
+        xhr.send();
+    }
+    console.log(methodType+" request send to server at "+showTime());
+});
+}
